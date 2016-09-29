bd9a6ac27abdf9f815278056f3211c6550d12f4f
Palash Chatterjee <palash.s.chatterjee@gmail.com>
2016-09-29 12:17:13 +0530

    Removed Toolbar

--- Sunshine/app/src/main/java/com/example/cpalash/sunshine/Home.java
+++ Sunshine/app/src/main/java/com/example/cpalash/sunshine/Home.java
@@ -11,16 +11,5 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
 }

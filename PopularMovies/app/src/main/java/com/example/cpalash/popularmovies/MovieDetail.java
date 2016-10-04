3accdef0a9f36e2cf1a30fae357de72cca354fdd
Palash Chatterjee <palash.s.chatterjee@gmail.com>
2016-10-04 15:41:30 +0530

    Fixed the layout of movie_detail.xml

--- PopularMovies/app/src/main/java/com/example/cpalash/popularmovies/MovieDetail.java
+++ PopularMovies/app/src/main/java/com/example/cpalash/popularmovies/MovieDetail.java
@@ -3,6 +3,7 @@ package com.example.cpalash.popularmovies;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +12,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -42,9 +46,25 @@ public class MovieDetail extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.movie_detail, container, false);
             Intent movieDetailsIntent = getActivity().getIntent();
             List<String> movieDetails = movieDetailsIntent.getStringArrayListExtra("details");
-            Log.v("MovieDetailFragment", movieDetails.toString());
-            TextView details = (TextView) rootView.findViewById(R.id.movie_detail_movie_name);
-            details.setText(movieDetails.toString());
+            String name = movieDetails.get(0);
+            String poster = movieDetails.get(1);
+            String synopsis = movieDetails.get(2);
+            String date = movieDetails.get(3);
+            String rating = movieDetails.get(4);
+
+            TextView nameTextView  = (TextView) rootView.findViewById(R.id.movie_detail_movie_name);
+            nameTextView.setText(name);
+
+            TextView dateTextView  = (TextView) rootView.findViewById(R.id.movie_detail_movie_release);
+            dateTextView.setText(date);
+
+            TextView ratingTextView  = (TextView) rootView.findViewById(R.id.movie_detail_movie_rating);
+            ratingTextView.setText(rating);
+
+            ImageView posterImageView = (ImageView) rootView.findViewById(R.id.movie_detail_movie_poster);
+            Picasso.with(getContext()).load(poster).resize(150,150).into(posterImageView);
+
+            Log.v("Movie data", movieDetails.toString());
             return rootView;
         }
     }
